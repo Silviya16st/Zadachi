de65724c26428d3e669dac9111c14581a781cd72
Silviya16st <silviyas225@gmail.com>
2024-10-22 20:25:22 +0300

    zadacha

---
+++ ListWithProducts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace ListWithProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList products = new ArrayList();
+            products.Add("papers");
+            products.Add("tomatoes");
+            products.Add("eggplants");
+            ArrayList prices = new ArrayList();
+            prices.Add(2.20);
+            prices.Add(1.50);
+            prices.Add(3.40);
+            ArrayList qty = new ArrayList();
+            qty.Add(5);
+            qty.Add(2);
+            qty.Add(1);
+
+        }
+    }
+}
